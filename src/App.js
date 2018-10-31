5d44bf33cf4be2cd36d31b927ee83c9b8ce01006
JON J <gr8fuldwg@gmail.com>
2018-10-31 18:42:17 -0400

    created the skeleton for the app component structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ class App extends Component {
   render() {
     return (
       <div>
-       <h1>Hello! It works!</h1>
+        <header className="Header">
+          <h1 className="title">Color Box</h1>
+        </header>
+        <main>
+          <div className="box"></div>
+          <input type="color" value="#ff0000" className="color-selector" />
+        </main>
+        <footer className="Footer">
+          <p>Made by me!</p>
+        </footer>
       </div>
     );
   }
